70354f559e0bbbe1a7c5a651b1251c802a850bfc
vanilla2wilight <dave.dongbofan@gmail.com>
2019-01-25 11:16:27 +0800

    For running in Automator /bin/bash

--- Automator/smart space.py
+++ Automator/smart space.py
@@ -1,11 +1,22 @@
+runpy3 () {
+/anaconda3/bin/python << 'EOF' - "$@"
+
+import sys
 import pangu
 import clipboard
 import pyautogui
-	
-target = clipboard.paste()
-result = pangu.spacing_text(target)
-clipboard.copy(result)
 
-pyautogui.keyDown('command')
-pyautogui.press('v')
-pyautogui.keyUp('command')
+for f in sys.argv:
+
+	target = clipboard.paste()
+	result = pangu.spacing_text(target)
+	clipboard.copy(result)
+
+	pyautogui.keyDown('command')
+	pyautogui.press('v')
+	pyautogui.keyUp('command')
+
+EOF
+}
+
+runpy3 "$@"
